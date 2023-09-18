da76b1d66bfad340bfa9a103c933f3c6b0dcf858
Igor Kroeber <igor.kroeber@gmail.com>
2023-09-18 15:41:24 -0300

    Initial Commit

---
+++ Example.ConsoleConsumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+
+ConsumerConfig config = new ConsumerConfig()
+{
+    BootstrapServers = "localhost:9094",
+    GroupId = "ExampleGroup",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+
+};
+
+IConsumer<string, string> consumer =
+    new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("ExampleTopic");
+
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+Console.CancelKeyPress += (_, e) =>
+{
+    cancellationTokenSource.Cancel();
+    e.Cancel = true;
+};
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\n--- Listening for incoming messages --- \n");
+
+Console.ResetColor();
+while (!cancellationToken.IsCancellationRequested)
+{
+    try
+    {
+
+        var result = consumer.Consume(cancellationToken);
+        Console.WriteLine($"Consumed \"{result.Message.Value}\"");
+    }
+    catch (OperationCanceledException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\n→ Cancelling consume operation");
+    }
+}
+
+Console.WriteLine("→ Closing consumer\n");
+Console.ResetColor();
+
+consumer.Close();
---
+++ Example.ConsoleProducer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+
+ProducerConfig config = new ProducerConfig()
+{
+    BootstrapServers = "localhost:9094",
+    Acks = Acks.Leader
+};
+
+IProducer<string, string> producer =
+    new ProducerBuilder<string, string>(config).Build();
+
+
+for (int i = 0; i < 100; i++)
+{
+    producer.Produce("ExampleTopic",
+        new Message<string, string>()
+        {
+            Key = $"Key: {i}",
+            Value = $"Value: {i}"
+        }
+    );
+    producer.Flush();
+    Console.WriteLine($"Produced \"Value: {i}\"");
+}
